3bed0d9f66727559c0b3de7a40d370b038bae474
FinchPowers <fm.lheureux@gmail.com>
2013-05-11 11:32:00 -0400

    Updated cake core

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -9,12 +9,13 @@
  * PHP 5
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
  * @package       app.Config
  * @since         CakePHP(tm) v 0.2.9
@@ -25,18 +26,20 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
+	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+/**
+ * ...and connect the rest of 'Pages' controller's urls.
+ */
+	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
-	Router::connect('/Menus', array('controller' => 'Menus', 'action' => 'index'));
-	Router::connect('/Menus/update', array('controller' => 'Menus', 'action' => 'update'));
-	Router::connect('/Menus/tools', array('controller' => 'Menus', 'action' => 'index', 'tools'));
-	Router::connect('/Menus/datamart', array('controller' => 'Menus', 'action' => 'index', 'datamart'));
-	
-	Router::connect('/Pages/*', array('controller' => 'Pages', 'action' => 'display'));
-	Router::connect('/', array('controller' => 'Users', 'action' => 'login'));
+/**
+ * Load all plugin routes. See the CakePlugin documentation on
+ * how to customize the loading of plugin routes.
+ */
+	CakePlugin::routes();
 
-	foreach(CakePlugin::loaded() as $plugin){
-		Router::connect('/'.$plugin.'/:controller', array('plugin' => $plugin, 'action' => 'index'));
-		Router::connect('/'.$plugin.'/:controller/:action/*', array('plugin' => $plugin));
-	}
-	
-	Router::connect('/:controller/:action/*');
+/**
+ * Load the CakePHP default routes. Only remove this if you do not want to use
+ * the built-in default routes.
+ */
+	require CAKE . 'Config' . DS . 'routes.php';
--- app/View/Layouts/flash.ctp
+++ app/View/Layouts/flash.ctp
@@ -1,31 +1,38 @@
-<!DOCTYPE HTML>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+/**
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.View.Layouts
+ * @since         CakePHP(tm) v 0.10.0.1076
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+?>
+<!DOCTYPE html>
+<html>
 <head>
-	
-	<title><?php echo $page_title.' &laquo; '.__('core_appname', true); ?></title>
-	<link rel="shortcut icon" href="<?php echo($this->webroot); ?>img/favicon.ico"/>
+<?php echo $this->Html->charset(); ?>
+<title><?php echo $page_title; ?></title>
 
-	<?php 
-		echo $this->Html->css('style');
-		echo $this->Html->charset('UTF-8');
-	 ?>
-
-	
+<?php if (Configure::read('debug') == 0) { ?>
+<meta http-equiv="Refresh" content="<?php echo $pause; ?>;url=<?php echo $url; ?>"/>
+<?php } ?>
+<style><!--
+P { text-align:center; font:bold 1.1em sans-serif }
+A { color:#444; text-decoration:none }
+A:HOVER { text-decoration: underline; color:#44E }
+--></style>
 </head>
-
-<body class="flash">
-
-    <div class="wrapper">
-		<a href="<?php echo $url; ?>"> 
-        	<?php echo __( $message, true ); ?>
-        	<br/>
-	        <small><?php __('click to continue'); ?></small>
-		</a>
-    </div>
-    <?php
-    	echo $this->element('sql_dump');
-    ?>
-    
+<body>
+<p><a href="<?php echo $url; ?>"><?php echo $message; ?></a></p>
 </body>
-
 </html>
